7c35fa4ce98fa18d827479f27e225d5e7f60ed29
David-odhiambo254 <davidodhiambookoth3@gmail.com>
2026-04-20 13:45:48 +0300

    Route_correction(case_sensitive)v1-V1

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\v1'], function () {
+Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1'], function () {
     Route::resource('customers', 'CustomerController'); //CustomerController::class
     Route::resource('files', 'FileController'); //FileController::class
     Route::resource('orders', 'OrderController'); //OrderController::class
